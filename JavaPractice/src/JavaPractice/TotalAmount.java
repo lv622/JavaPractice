362bb6e66caed40c2bdbd17e5702a3692a884753
hrkim <hrkim@LAPTOP-SE4R69IP>
2021-06-09 14:56:05 +0900

    Practice #2

---
+++ JavaPractice/src/JavaPractice/TotalAmount.java
@@ -0,0 +1,15 @@
+package JavaPractice;
+
+public class TotalAmount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum = 0;
+
+		for (int number = 0; number <= 100; number++) {
+			sum += number;
+		}
+		System.out.println("합계: " + sum);
+	}
+
+}
