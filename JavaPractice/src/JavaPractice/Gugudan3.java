15d28ecf628391fdd4f6f926de940dfe26c74fb3
hrkim <hrkim@LAPTOP-SE4R69IP>
2021-06-09 14:57:45 +0900

    Practice #3-1

---
+++ JavaPractice/src/JavaPractice/Gugudan3.java
@@ -0,0 +1,16 @@
+package JavaPractice;
+
+public class Gugudan3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 3;
+		int result = 0;
+
+		for (int i = 1; i < 10; i++) {
+			result = num * i;
+			System.out.println(num + " x " + i + " = " + result);
+		}
+	}
+
+}
